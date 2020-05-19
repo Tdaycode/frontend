46b77d67749ae9472b21619073168119ab5297bb
Tdaycode <60110209+Tdaycode@users.noreply.github.com>
2020-05-19 06:50:21 +0100

    Add files via upload

---
+++ projecDice/external.js
@@ -0,0 +1,28 @@
+// first random image code
+var randomNumber1 = Math.floor((Math.random(1) * 6) + 1);
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+var randomImageSource = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+// second random imge code shorter form
+
+var randomNumber2 = Math.floor(Math.random(1) * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+// if player wins or loose
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "player 1 wins!🚩"
+} else if (randomNumber2 > randomNumber1) { document.querySelector("h1").innerHTML = "player 2 wins!🚩" } //if draw
+else {
+    document.querySelector("h1").innerHTML = "oh draw!"
+
+}
